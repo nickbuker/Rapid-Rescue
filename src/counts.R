56076d2f00949e29cf3acd9779e9b97e7e1622ea
nickbuker <nickbuker@gmail.com>
2016-11-15 17:49:27 -0800

    add R counter

---
+++ src/counts.R
@@ -0,0 +1,8 @@
+library(plyr)
+
+model_data <- read.csv("~/Documents/DSI/DSI_capstone/data/model_data_no_out.csv")
+
+model_data_counted = count(model_data, vars = c('mariners_home', 'seahawks_home', 'sounders_home', 'trav_holiday', 'dang_holiday', 'night', 'Monday', 'Saturday', 'Sunday', 'Thursday', 'Tuesday', 'Wednesday', 'day_num', 'zone1', 'zone2', 'zone3', 'zone4', 'zone5', 'zone6', 'zone7', 'seasonality'))
+
+write.csv(model_data_counted, file = 'model_data_counted_no_out.csv')
+
