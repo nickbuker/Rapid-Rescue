4a74f285d50fe72251102def581c5a5d18a3ccb3
nickbuker <nickbuker@gmail.com>
2016-11-17 17:02:25 -0800

    update app

--- app/response_app.py
+++ app/response_app.py
@@ -11,6 +11,18 @@ def index():
 def predict():
     pass
 
+@app.route('/model')
+def model():
+    return render_template('model.html', title='Rapid Rescue')
+
+@app.route('/me')
+def me():
+    return render_template('me.html', title='Rapid Rescue')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html', title='Rapid Rescue')
+
 # Be careful with debug!
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080, debug=True)
